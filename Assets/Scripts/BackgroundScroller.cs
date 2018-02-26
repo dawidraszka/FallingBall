f32f518306bae08697f690c1b7c50e50b963aa7e
Unknown <raszka.dawid@gmail.com>
2018-02-26 20:46:59 +0100

    Add scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	public float scrollingSpeed = 0.75f;
+
+	private float lenght;
+	private Vector3 startingPosition;
+
+	void Start ()
+	{
+		lenght = transform.localScale.y / 2;
+
+		startingPosition = transform.localPosition;
+	}
+	
+	void Update ()
+	{  
+		float newPosition = Mathf.Repeat(Time.time * scrollingSpeed, lenght);
+
+		transform.localPosition = startingPosition + Vector3.up * newPosition;
+	}
+}
