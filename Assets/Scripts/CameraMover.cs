119be80d105840084b4dccb96b87987bed15bced
Unknown <raszka.dawid@gmail.com>
2018-02-26 19:53:38 +0100

    Add camera movement

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+
+	public float speed = 0.75f;
+	
+	void Update () {
+		transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+	}
+
+}
